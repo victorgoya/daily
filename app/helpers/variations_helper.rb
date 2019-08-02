d996a8953138f428a7323ab94c4d12e5fb7771c0
Victor Goya <goya.victor@gmail.com>
2019-08-02 20:36:19 +0200

    small style fixes

--- app/helpers/variations_helper.rb
+++ app/helpers/variations_helper.rb
@@ -6,4 +6,9 @@ module VariationsHelper
   def current_daily_budget
     @current_user.daily_budget - @variations.map(&:daily_value).sum
   end
+
+  def format_value(value)
+    money = Money.new(value * 100, @current_user.currency)
+    money.format(symbol_position: money.currency.to_s === "EUR" ? :after : :before)
+  end
 end
---
+++ config/initializers/money.rb
@@ -0,0 +1,6 @@
+MoneyRails.configure do |config|
+  config.default_format = {
+    no_cents_if_whole: true,
+  }
+end
+
