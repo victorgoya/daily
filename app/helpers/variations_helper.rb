46de8ed9a919856235c2418a42e39dcbe8d55734
Victor Goya <goya.victor@gmail.com>
2019-08-02 23:11:45 +0200

    add savings count

--- app/helpers/variations_helper.rb
+++ app/helpers/variations_helper.rb
@@ -1,14 +1,18 @@
 module VariationsHelper
   def current_monthly_budget
-    @current_user.monthly_budget - @variations.map(&:monthly_value).sum
+    @current_monthly_budget ||= @current_user.monthly_budget - @variations.map(&:monthly_value).sum
   end
 
   def current_daily_budget
-    @current_user.daily_budget - @variations.map(&:daily_value).sum
+    current_daily_budget ||= @current_user.daily_budget - @variations.map(&:daily_value).sum
   end
 
   def format_value(value)
     money = Money.new(value * 100, @current_user.currency)
     money.format(symbol_position: money.currency.to_s === "EUR" ? :after : :before)
+  end
+
+  def saved_this_month
+    @saved_this_month ||= @current_user.daily_budget * (Time.now.day - 1) - @current_user.variations.from_this_month.map(&:daily_value).sum
   end
 end
