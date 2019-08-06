a10f35045d07b0044f16de7b19e9001ac42d6174
Victor Goya <goya.victor@gmail.com>
2019-08-06 15:21:51 +0200

    try to fix timezones

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_one :base_variation, -> (user) { user.variations.where(base: true) }, class_name: "Variation"
 
   def daily_budget
-    (monthly_budget || 0) / Time.now.end_of_month.day
+    (monthly_budget || 0) / Time.zone.now.end_of_month.day
   end
 end
--- app/models/variation.rb
+++ app/models/variation.rb
@@ -4,12 +4,12 @@ class Variation < ApplicationRecord
   validates :value, presence: true
   validates :label, presence: true
 
-  scope :from_today, -> { where("(variations.created_at > ? OR variations.recurring = ?) AND variations.base = ?", Time.now.beginning_of_day, true, false) }
-  scope :from_this_month, -> { where("variations.created_at > ? OR variations.recurring = ?", Time.now.beginning_of_month, true) }
+  scope :from_today, -> { where("(variations.created_at > ? OR variations.recurring = ?) AND variations.base = ?", Time.zone.now.beginning_of_day, true, false) }
+  scope :from_this_month, -> { where("variations.created_at > ? OR variations.recurring = ?", Time.zone.now.beginning_of_month, true) }
 
   def daily_value
     if self.recurring?
-      value / Time.now.end_of_month.day
+      value / Time.zone.now.end_of_month.day
     else
       value
     end
