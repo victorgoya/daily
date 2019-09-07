1f795d7e2c85be962e93083723a4afd57d171b88
Victor Goya <goya.victor@gmail.com>
2019-09-08 01:11:40 +0200

    fix daily overspent

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_one :base_variation, -> (user) { user.variations.where(base: true) }, class_name: "Variation"
 
   def daily_budget
-    ((monthly_budget - variations.find_by(base: true)&.value) || 0) / Time.zone.now.end_of_month.day
+    ((monthly_budget - variations.from_this_month.find_by(base: true)&.value) || 0) / Time.zone.now.end_of_month.day
   end
 end
