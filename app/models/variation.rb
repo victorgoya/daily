f12d15eac65a9847ad5029f2e4d9d4532a760adb
Victor Goya <goya.victor@gmail.com>
2019-09-22 20:42:08 +0200

    fix variation range

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -4,7 +4,7 @@ class Variation < ApplicationRecord
   validates :value, presence: true
   validates :label, presence: true
 
-  scope :from_today, -> (date) { where("((? < variations.created_at + variations.spread * interval '1 day' AND ? > variations.created_at) OR variations.recurring = ?) AND variations.base = ?", date.beginning_of_day, date.beginning_of_day, true, false) }
+  scope :from_today, -> (date) { where("((? < variations.created_at + variations.spread * interval '1 day' AND ? > variations.created_at) OR variations.recurring = ?) AND variations.base = ?", date.end_of_day, date.end_of_day, true, false) }
   scope :from_this_month, -> (date) { where("variations.created_at > ? OR variations.recurring = ?", date.beginning_of_month, true) }
 
   def daily_value(date)
